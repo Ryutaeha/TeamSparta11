4995aeb6bd1e6b2d6561e03b096e97a054125d36
taeseongHan <taeseong0505@naver.com>
2023-11-17 19:56:57 +0900

    2023-11-17 한태성
    플레이어 정보
    몬스터 정보
    몬스터 종류 정리
    플레이어 직업 정리

---
+++ TeamSparta11/MonsterStatus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Teamproject
+{
+    //몬스터
+    internal class MonsterStatus : ICharacter
+    {
+        public string Name { get; }
+        public int MaxHP { get; set; }
+        public int HP { get; set; }
+        public int Speed { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; }
+        public int AD { get; set; }
+        public int DF { get; set; }
+        public int EXP { get; set; }
+        public bool IsDead => HP <= 0;
+
+
+        public MonsterStatus(string name, int level, int maxhp, int hp, int ad, int df, int speed, int exp, int gold)
+        {
+            Name = name;
+            Level = level;
+            MaxHP = maxhp;
+            HP = hp;
+            Speed = speed;
+            Gold = gold;
+            AD = ad;
+            DF = df;
+            EXP = exp;
+
+        }
+    }
+
+
+
+
+
+    //보스 몬스터
+    internal class BossMonsterStatus : ICharacter
+    {
+        public string Name { get; }
+        public int MaxHP { get; set; }
+        public int HP { get; set; }
+        public int Speed { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
+        public int AD { get; set; }
+        public int DF { get; set; }
+        public int EXP { get; set; }
+        public bool IsDead => HP <= 0;
+
+
+        public int MaxMP { get; set; }
+        public int MP { get; set; }
+        public string DropItem { get; set; }
+
+        public BossMonsterStatus(string name, int level, int maxHP, int hp, int maxMP, int mp, int ad, int df, int exp, int gold, string dropitem)
+        {
+            Name = name;
+            Level = level;
+            MaxHP = maxHP;
+            HP = hp;
+            MaxMP = maxMP;
+            MP = mp;
+            AD = ad;
+            DF = df;
+            EXP = exp;
+            Gold = gold;
+            DropItem = dropitem;
+
+        }
+
+
+    }
+}
---
+++ TeamSparta11/PlayerStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Teamproject
+{
+    internal class PlayerStatus : ICharacter
+    {
+        public string Name { get; }
+        public int MaxHP { get; set; }
+        public int HP { get; set; }
+        public int Speed { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; }
+        public int AD { get; set; }
+        public int DF { get; set; }
+        public int EXP { get; set; }
+        public bool IsDead => HP <= 0;
+
+        public string Job { get; }
+        public int MaxMP { get; set; }
+        public int MP { get; set; }
+
+        public PlayerStatus(string name, string job, int ad, int df, int exp)
+        {
+            Name = name;
+            Job = job;
+            Level = 1;
+            MaxHP = 100;
+            HP = 100;
+            MaxMP = 50;
+            MP = 50;
+            Speed = 1;
+            Gold = 100;
+            AD = ad;
+            DF = df;
+            EXP = exp;
+            
+
+        }
+    }
+}
