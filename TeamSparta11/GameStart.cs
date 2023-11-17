42fac29c77c39c71540a38006035060774edde28
hyeon-tak <149382994+hyeon-tak@users.noreply.github.com>
2023-11-17 10:19:41 +0900

    2023-11-17오현탁
    시작화면을 강의내용과 동일하게 해놨습니다 색을 할 줄 몰라서 안했습니다

---
+++ TeamSparta11/GameStart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSparta11
+{
+    internal class GameStart
+    {
+        internal void Game()
+        {
+            Console.WriteLine("스파르타 던전에 오신 여러분을 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("전트시작");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 선택해주세요");
+            Console.WriteLine(">>");
+
+
+        }
+    }
+}
