1fd8927dd08f58a6f81eb6993db4516aa056648d
taeseongHan <taeseong0505@naver.com>
2023-11-17 11:20:35 +0900

    2023-11-17 taeseong
    playerstatus.cs 추가함 interface씀
    Monsterstatus.cs

---
+++ Teamproject/Teamproject/Date.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 몬스터 정보나 기타 등등의 일반적인 데이터를 담는 클래스
+internal static class Date
+{
+    public 
+}
+
