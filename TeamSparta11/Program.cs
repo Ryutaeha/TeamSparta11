b5dafe903105d1afd6c8c094e0429c1e932f6f19
Ryutaeha <taeha61@gmail.com>
2023-11-22 05:09:04 +0900

    2023-11-22 유태하
    배틀씬 구현
    일반 몬스터 및 보스 몬스터 구현 스테이지 구현
    치명타 구현
    속도별 순서 구현
    전투 종료시 골드 및 경험치 획득 구현
    장착아이템 스탯 더하기 미구현
    스테이지 종료시 체력 회복 구현
    
    혹시 몰라서 만들어 놨어요 참고 할거 있으면 참고하시면서 하면 좋을거 같아용!

--- TeamSparta11/Program.cs
+++ TeamSparta11/Program.cs
@@ -9,7 +9,7 @@ namespace TeamSparta11
         {
             // 아이템 데이터 세팅 함수
             Date.ItemDateTableSetting();
-           
+
             //Static이 아닌 메서드 구현 후 각자의 작업물은 해당 아래서 인스턴스 생성후 호출해서 테스트 하시면 됩니다
             // EX) 
             //GameStart 인스턴스화
