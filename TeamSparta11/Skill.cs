a7d51f7823c2c60b66f270680e6bfdffe0b5a97c
hyeon-tak <149382994+hyeon-tak@users.noreply.github.com>
2023-11-21 11:21:25 +0900

    2023-11-21오현탁
    player 클래스에서 받던 거 세이브테이터의 정보 받아오게 변경 => PlayerInfo.Player

--- TeamSparta11/Skill.cs
+++ TeamSparta11/Skill.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -7,15 +6,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using Teamproject;
 using static System.Net.Mime.MediaTypeNames;
-
 internal class Skill : ISkill
 {
     public string Name { get; }
-    public int Ability { get; set;}
+    public int Ability { get; set; }
     public int AbilityPower => new Random().Next(0, 10) + Ability;
     public int Cost { get; }
     public string SkillInfo { get; }
-
     public Skill(string name, int ability, int cost, string skillInfo)
     {
         Name = name;
@@ -23,6 +20,19 @@ internal class Skill : ISkill
         Cost = cost;
         SkillInfo = skillInfo;
     }
-
+    //몬스터 공격
+    public void Use(MonsterStatus monster)
+    {
+        PlayerInfo.Player.MP -= Cost;
+        int damage = AbilityPower + PlayerInfo.Player.AD - monster.DF;
+        monster.HP -= damage;
+        Console.WriteLine($"{monster.Name}에게 {Name}(으)로 {damage}의 피해를 입혔습니다.");
+    }
+    public void Use(BossMonsterStatus bossMonster)
+    {
+        PlayerInfo.Player.MP -= Cost;
+        int damage = AbilityPower + PlayerInfo.Player.AD - bossMonster.DF;
+        bossMonster.HP -= damage;
+        Console.WriteLine($"{bossMonster.Name}에게 {Name}(으)로 {damage}의 피해를 입혔습니다.");
+    }
 }
-
