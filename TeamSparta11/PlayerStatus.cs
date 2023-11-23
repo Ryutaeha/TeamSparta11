eb3e5c3f05536cd26bcedf20e73e35e3a106f152
Ryutaeha <taeha61@gmail.com>
2023-11-23 13:24:50 +0900

    2023-11-23 유태하
    플레이어 치명타 피해 수정

--- TeamSparta11/PlayerStatus.cs
+++ TeamSparta11/PlayerStatus.cs
@@ -83,7 +83,7 @@ namespace Teamproject
         }
         public void BeFatalDamaged(int damage)
         {
-            int dameges = damage - DF;
+            int dameges = (damage * 2) - DF;
             if (dameges < 0) damage = 1;
             HP -= damage;
             Console.WriteLine($"{Name}(은)는 {damage}의 치명적 피해를 입었습니다. 남은 체력 : {HP}");
